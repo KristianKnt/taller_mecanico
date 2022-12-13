feb7218e5ae1d4824ee8e8ee0c5c4e4f5610d595
Kristian Kmilo <cristian.rojascapera5@gmail.com>
2022-12-13 16:44:29 -0500

    cambio del host

--- pagina/layout/dbcon.php
+++ pagina/layout/dbcon.php
@@ -8,8 +8,8 @@ $dbname = 'taller_mecanico';
 $con = mysqli_connect($host,$dbuser,$dbpass,$dbname);
 */
 
-//hecho para jlastic
-$host = 'node122447-env-2831420.jelastic.saveincloud.net';
+//hecho para jlastic node122447-env-2831420.jelastic.saveincloud.net
+$host = '127.0.0.1';
 $dbuser = 'root';
 $dbpass = 'GZQfet63329';
 $dbname = 'taller_mecanico';
