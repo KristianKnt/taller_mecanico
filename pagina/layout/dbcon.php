ad66ba4adf2bc905027c5fd26d7a204f9a07cc4c
Kristian Kmilo <cristian.rojascapera5@gmail.com>
2022-12-13 17:22:16 -0500

    se revierte el utlimo cambio

--- pagina/layout/dbcon.php
+++ pagina/layout/dbcon.php
@@ -9,7 +9,7 @@ $con = mysqli_connect($host,$dbuser,$dbpass,$dbname);
 */
 
 //hecho para jlastic node122447-env-2831420.jelastic.saveincloud.net
-$host = '127.0.0.1';
+$host = 'node122447-env-2831420.jelastic.saveincloud.net';
 $dbuser = 'root';
 $dbpass = 'GZQfet63329';
 $dbname = 'taller_mecanico';
